d386cf945455dc8dc72c93b0545f7717990cc7b3
Adrien Turiot <didibear77@gmail.com>
2018-02-23 00:49:10 +0100

    Manage pathfinding dfs

--- gym_pathfinding/games/pathfinding.py
+++ gym_pathfinding/games/pathfinding.py
@@ -15,8 +15,6 @@ class PathFindingGame(object):
         2 = player
         3 = goal
     """
-    # North, South, East, West
-    MOUVEMENT = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 
     def __init__(self, width=15, height=15, seed=None):
         self.width = width
@@ -49,10 +47,9 @@ class PathFindingGame(object):
         xs, ys = np.where(self.grid == 0)
         free_positions = list(zip(xs, ys))
 
-        target_spawn, player_spawn = self.rng.sample(free_positions, 2)
+        player_spawn, target_spawn = self.rng.sample(free_positions, 2)
 
-        # self.optimal_path_length, self.optimal_path = self.dfs(player_spawn, target_spawn)
-
+        np.max()
         return player_spawn, target_spawn
 
     def get_state(self):
@@ -68,12 +65,12 @@ class PathFindingGame(object):
         
         assert 0 <= a and a < 4
 
-        dx, dy = self.MOUVEMENT[a]
+        dx, dy = MOUVEMENT[a]
         px, py = self.player
 
         next_x, next_y = px + dx, py + dy
 
-        if self.is_legal(next_x, next_y):
+        if is_legal(self.grid, next_x, next_y):
             self.player = (next_x, next_y)
 
         if self.player == self.target:
@@ -83,34 +80,45 @@ class PathFindingGame(object):
         return self.step_return(-0.01)
 
     def step_return(self, reward):
-        return self.get_state(), reward, self.terminal, {}
-        #{"optimal_path": self.optimal_path_length}
+        return self.get_state(), reward, self.terminal, ""
+        
+# North, South, East, West
+MOUVEMENT = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+
+def is_legal(grid, next_x, next_y):
+    return grid[next_x, next_y] == 0
 
 
-    def is_legal(self, next_x, next_y):
-        return self.grid[next_x, next_y] == 0
 
+def dfs(grid, start, goal):
+    """ Depth-first search on the grid
 
-    # def dfs(self, start, goal):
-    #     stack = [(start, [start])]
+    return :
+        path_length : the path length
+        path : list of positions
+    """
 
-    #     possible_path = PriorityQueue()
+    stack = [(start, [start])]
 
-    #     while stack:
-    #         (vertex, path) = stack.pop()
-    #         legal_cells = set(self.legal_directions(*vertex)) - set(path)
-    #         for next in legal_cells:
-    #             if next == goal:
-    #                 full_path = path + [next]
-    #                 length = len(path)
-    #                 possible_path.put((length, full_path))
-    #             else:
-    #                 stack.append((next, path + [next]))
+    possible_path = PriorityQueue()
+    visited = set()
+    while stack:
+        (vertex, path) = stack.pop()
+        visited.add(vertex)
 
-    #     return possible_path.get()
+        legal_cells = set(legal_directions(grid, *vertex)) - visited
+        for next in legal_cells:
+            if next == goal:
+                full_path = path + [next]
+                length = len(path)
+                possible_path.put((length, full_path))
+            else:
+                stack.append((next, path + [next]))
 
-    # def legal_directions(self, posx, posy):
-    #     possible_moves = [(posx + dx, posy + dy) for dx, dy in self.MOUVEMENT]
-    #     return [(next_x, next_y) for next_x, next_y in possible_moves if self.is_legal(next_x, next_y)]
+    return possible_path.get()
+
+def legal_directions(grid, posx, posy):
+    possible_moves = [(posx + dx, posy + dy) for dx, dy in MOUVEMENT]
+    return [(next_x, next_y) for next_x, next_y in possible_moves if is_legal(grid, next_x, next_y)]
 
 
---
+++ test_view.py
@@ -0,0 +1,18 @@
+import gym
+import gym_pathfinding
+from time import sleep
+
+env = gym.make('pathfinding-9x9-v0')
+
+for episode in range(5):
+    s = env.reset()
+    
+    for timestep in range(50):
+        env.render()
+        sleep(0.05)
+        
+        s, r, done, _ = env.step(env.action_space.sample())
+        
+        if done:
+            break
+        
